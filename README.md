eb00134b4b024eceee4e46be60509a04c0507880
Claudio Bisegni <bisegni@slac.stanford.edu>
2023-11-14 11:45:18 -0800

    add defautl readme and license

---
+++ README.md
@@ -0,0 +1,18 @@
+![logo](./logos/SLAC-lab-hires.png)
+# Base lib for AD-EED java microservices
+
+This is a proposed template for SLAC software development.  
+We hope that this template can be used to encourage best practices in both code organization and SLAC/Stanford collaboration compliance.  
+The default state is set to 'private' but we are encouraging open sharing of DOE funded developments with funding attribution and potentially hosting at [OSTI DOE-CODE](https://www.osti.gov/doecode/).  
+
+# SLAC National Accelerator Laboratory
+The SLAC National Accelerator Laboratory is operated by Stanford University for the US Departement of Energy.  
+[DOE/Stanford Contract](https://legal.slac.stanford.edu/sites/default/files/Conformed%20Prime%20Contract%20DE-AC02-76SF00515%20as%20of%202022.10.01.pdf)
+
+# Lisence
+
+Copyright (c) 2017-2020, The Board of Trustees of the Leland Stanford Junior University, through SLAC National Accelerator Laboratory... the complete license is [here](LICENSE.md)  
+
+
+
+
--- src/main/java/edu/stanford/salc/slacadeedbaselib/dto/ApiResultResponse.java
+++ src/main/java/edu/stanford/salc/slacadeedbaselib/dto/ApiResultResponse.java
@@ -18,9 +18,9 @@ public class ApiResultResponse<T> {
     @Builder.Default
     @Schema(description = "Is the error code returned from api", requiredMode = Schema.RequiredMode.REQUIRED)
     private int errorCode = 0;
-    @Schema(description = "In case of error not equal to 0, an error message can be reported by api, indicating what problem is occured")
+    @Schema(description = "In case of error not equal to 0, an error message can be reported by api, indicating what problem is occurred")
     private String errorMessage;
-    @Schema(description = "In case of error not equal to 0, an error domain can be reported by api, indicating where the problem is occured")
+    @Schema(description = "In case of error not equal to 0, an error domain can be reported by api, indicating where the problem is occurred")
     private String errorDomain;
     @Schema(description = "Is the value returned by api")
     private T payload;
@@ -30,7 +30,7 @@ public class ApiResultResponse<T> {
      *
      * @param errorCode    the error code
      * @param errorMessage the error message
-     * @param errorDomain  the error domani, where the error is occurs
+     * @param errorDomain  the error domain, where the error is occurs
      * @param <T>          the type
      * @return return the instance with error information
      */
