11ae5164ce821c15d6f3eb0e10c8efa31cbcaf6e
Claudio Bisegni <bisegni@slac.stanford.edu>
2023-11-16 14:42:42 -0800

    fixes

--- src/main/java/edu/stanford/slac/ad/eed/baselib/config/InitLDAP.java
+++ src/main/java/edu/stanford/slac/ad/eed/baselib/config/InitLDAP.java
@@ -4,6 +4,6 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.data.ldap.repository.config.EnableLdapRepositories;
 
 @Configuration
-@EnableLdapRepositories(basePackages = "edu.stanford.slac.ldap_repository")
+@EnableLdapRepositories(basePackages = "edu.stanford.slac.ad.eed.baselib.repository")
 public class InitLDAP {
 }
