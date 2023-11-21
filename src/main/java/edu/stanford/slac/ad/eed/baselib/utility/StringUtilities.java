eda41f37e85b5b25eaeb4ea7d88c6261ec931126
Claudio Bisegni <bisegni@slac.stanford.edu>
2023-11-21 09:12:17 -0800

    fix the normalization string utility

--- src/main/java/edu/stanford/slac/ad/eed/baselib/utility/StringUtilities.java
+++ src/main/java/edu/stanford/slac/ad/eed/baselib/utility/StringUtilities.java
@@ -2,22 +2,29 @@ package edu.stanford.slac.ad.eed.baselib.utility;
 
 
 import java.text.Normalizer;
+import java.util.Objects;
 
 public class StringUtilities {
-    static public String tokenNameNormalization(String tagName) {
-        return Normalizer
-                .normalize(
-                        tagName.trim(),
-                        Normalizer.Form.NFD
-                ).replaceAll("[^\\p{ASCII}]", "");
-    }
-
-    public static String authenticationTokenNormalization(String name) {
+    /**
+     * Normalize the input string to be all in lowercase and
+     * to be composed only by ascii character and at the end
+     * replace the space with the dash
+     * @param name is the string to convert
+     * @param stringToReplace this is the string to replace
+     * @param targetString this is the string used for the replace operation
+     * @return the normalized string
+     */
+    public static String normalizeStringWithReplace(String name, String stringToReplace, String targetString) {
         return Normalizer
                 .normalize(
                         name.trim().toLowerCase(),
                         Normalizer.Form.NFD
-                ).replaceAll("[^\\p{ASCII}]", "")
-                .replaceAll(" ", "-");
+                ).replaceAll(
+                        "[^\\p{ASCII}]",
+                        "")
+                .replaceAll(
+                        Objects.requireNonNull(stringToReplace),
+                        Objects.requireNonNull(targetString)
+                );
     }
 }
