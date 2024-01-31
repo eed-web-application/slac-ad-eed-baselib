38692111486ac4eb23ac8f6e0a64b5d4b01b81c0
Claudio Bisegni <bisegni@slac.stanford.edu>
2024-01-31 13:11:38 -0800

    token email now has appname befor the slac.app$ postfix in the email

--- src/main/java/edu/stanford/slac/ad/eed/baselib/api/v1/mapper/AuthMapper.java
+++ src/main/java/edu/stanford/slac/ad/eed/baselib/api/v1/mapper/AuthMapper.java
@@ -35,13 +35,13 @@ public abstract class AuthMapper {
     @Mapping(target = "authorizationType", expression = "java(Authorization.Type.valueOf(a.authorizationType().name()).getValue())")
     public abstract Authorization toModel(NewAuthorizationDTO a);
     public abstract List<Authorization> toModel(List<AuthorizationDTO> a);
-    @Mapping(target = "email", expression = "java(EMAIL_FORMAT.formatted(a.name(), appProperties.getApplicationTokenEmailDomain()))")
+    @Mapping(target = "email", expression = "java(EMAIL_FORMAT.formatted(a.name(), appProperties.getAppEmailPostfix()))")
     public abstract AuthenticationToken toModelAuthenticationToken(AuthenticationTokenDTO a);
-    @Mapping(target = "email", expression = "java(EMAIL_FORMAT.formatted(a.name(), appProperties.getApplicationTokenEmailDomain()))")
+    @Mapping(target = "email", expression = "java(EMAIL_FORMAT.formatted(a.name(), appProperties.getAppEmailPostfix()))")
     public abstract AuthenticationToken toModelApplicationToken(NewAuthenticationTokenDTO a);
-    @Mapping(target = "email", expression = "java(EMAIL_FORMAT.formatted(a.name(), appProperties.getAuthenticationTokenDomain()))")
+    @Mapping(target = "email", expression = "java(EMAIL_FORMAT.formatted(a.name(), appProperties.getAppEmailPostfix()))")
     public abstract AuthenticationToken toModelGlobalToken(AuthenticationTokenDTO a);
-    @Mapping(target = "email", expression = "java(EMAIL_FORMAT.formatted(a.name(), appProperties.getAuthenticationTokenDomain()))")
+    @Mapping(target = "email", expression = "java(EMAIL_FORMAT.formatted(a.name(), appProperties.getAppEmailPostfix()))")
     public abstract AuthenticationToken toModelGlobalToken(NewAuthenticationTokenDTO a);
     public abstract AuthenticationTokenDTO toTokenDTO(AuthenticationToken a);
     public abstract Authorization.Type toModel(AuthorizationTypeDTO type);
