5f3b35f80d6aeac2c3ee88f220ae545e85b7abb8
Claudio Bisegni <bisegni@slac.stanford.edu>
2024-06-25 16:05:51 -0700

    add lombok utility

--- src/main/java/edu/stanford/slac/ad/eed/baselib/api/v2/dto/AuthorizationGroupManagementDTO.java
+++ src/main/java/edu/stanford/slac/ad/eed/baselib/api/v2/dto/AuthorizationGroupManagementDTO.java
@@ -1,9 +1,14 @@
 package edu.stanford.slac.ad.eed.baselib.api.v2.dto;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonInclude;
 import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.Builder;
 
 import java.util.List;
-
+@Builder(toBuilder = true)
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@JsonIgnoreProperties(ignoreUnknown = true)
 @Schema(description = "Group management authorization")
 public record AuthorizationGroupManagementDTO(
         @Schema(description = "List of the user to add as authorized to manage group")
