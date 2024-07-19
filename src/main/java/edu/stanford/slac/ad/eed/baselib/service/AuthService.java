7ee00a9ef5dc6352d903c267de905a365bc719f8
Claudio Bisegni <bisegni@slac.stanford.edu>
2024-07-19 14:21:45 -0700

    fixes

--- src/main/java/edu/stanford/slac/ad/eed/baselib/service/AuthService.java
+++ src/main/java/edu/stanford/slac/ad/eed/baselib/service/AuthService.java
@@ -188,6 +188,7 @@ public abstract class AuthService {
      *
      * @param owner the owner
      */
+    @Cacheable(value = "user-authorization", key = "{#owner, #ownerType, #allHigherAuthOnSameResource}")
     public List<AuthorizationDTO> getAllAuthenticationForOwner(String owner, AuthorizationOwnerTypeDTO ownerType, Optional<Boolean> allHigherAuthOnSameResource) {
         return getAllAuthenticationForOwner(owner, ownerType, allHigherAuthOnSameResource, Optional.of(true));
     }
